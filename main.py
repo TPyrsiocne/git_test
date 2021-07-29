fdb5d811d18513f70f2c8f2fdf85879354c25b11
Tyson Price <tysonprice@Tysons-MacBook-Pro.local>
2021-07-28 19:11:58 -0700

    wote and tested isprime function. Modified main to only print squres of prime numbers in range 1 to 100

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 #a python program
+
+def isprime(n):
+	for i in range(2,n):
+		if n%i == 0:
+			return False
+		return True
+
 for i in range(1, 101):
-	print(i**2)
+	if isprime(i):
+		print(i**2)
+
+
+
